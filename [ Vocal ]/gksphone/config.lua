eae567adc5530142c4fa11a2fa736144de5f87e3
Benjamin7222 <benjidu722@gmail.com>
2023-05-13 22:38:12 +0200

    vocal

---
+++ [ Vocal ]/gksphone/config.lua
@@ -0,0 +1,37 @@
+
+
+Config = {}
+Config.OpenPhone = 'f2'   --## Phone open key ## https://docs.fivem.net/docs/game-references/input-mapper-parameter-ids/keyboard/
+Config.Phones = {"phone"}  -- # item name
+Config.Locale = 'en'
+Config.Fahrenheit = false
+
+Config.ESXVersion = "1.2" -- 1.1 OR 1.2
+
+
+-- Discord WebHook - start --
+Config.Carseller = 'https://discord.com/api/webhooks/1063424394430521384/wxl6LctOEU7p2KFCqBNhK7VZxNH5NRKQ8DW_ixtn7TbaqV7tmRGH-GyqHDRfRpIq17rR'
+Config.JobNotif = 'https://discord.com/api/webhooks/1063424394430521384/wxl6LctOEU7p2KFCqBNhK7VZxNH5NRKQ8DW_ixtn7TbaqV7tmRGH-GyqHDRfRpIq17rR'
+Config.TwitterWeb = 'https://discord.com/api/webhooks/1063424394430521384/wxl6LctOEU7p2KFCqBNhK7VZxNH5NRKQ8DW_ixtn7TbaqV7tmRGH-GyqHDRfRpIq17rR'
+Config.YellowWeb = 'https://discord.com/api/webhooks/1063424394430521384/wxl6LctOEU7p2KFCqBNhK7VZxNH5NRKQ8DW_ixtn7TbaqV7tmRGH-GyqHDRfRpIq17rR'
+Config.InstagramWeb = 'https://discord.com/api/webhooks/1063424394430521384/wxl6LctOEU7p2KFCqBNhK7VZxNH5NRKQ8DW_ixtn7TbaqV7tmRGH-GyqHDRfRpIq17rR'
+Config.Crypto = 'https://discord.com/api/webhooks/1063424394430521384/wxl6LctOEU7p2KFCqBNhK7VZxNH5NRKQ8DW_ixtn7TbaqV7tmRGH-GyqHDRfRpIq17rR'
+Config.BankTrasnfer = 'https://discord.com/api/webhooks/1063424394430521384/wxl6LctOEU7p2KFCqBNhK7VZxNH5NRKQ8DW_ixtn7TbaqV7tmRGH-GyqHDRfRpIq17rR'
+Config.BankLimit = 5000 -- # Minimum money transfer for discord webhook
+-- Discord WebHook - end --
+
+
+
+-- Phone Settings - Start --
+
+Config.UseMumbleVoIP    = false -- Use Frazzle's Mumble-VoIP Resource (Recomended!) https://github.com/FrazzIe/mumble-voip
+Config.PMAVoice         = true
+Config.UseTokoVoIP      = false
+Config.SaltyChat        = false
+
+Config.CallPhone        = true  -- If the player is not in the game or there is no item on it, it will give a warning.
+
+-- Phone Settings - Finish --
+
+Config.ValePrice        = 100    -- Vale Price
+Config.TaxiPrice        = 75     -- Taxi Price ( 75$/KM )
