e02017d7cbf675bf970c53dbcf0798c4fb6927d6
BobbyLaMalla <133542355+BobbyLaMalla@users.noreply.github.com>
2023-05-15 03:38:55 +0200

    StuntMoto

---
+++ [ Scripts ]/frobski-stunts/config.lua
@@ -0,0 +1,27 @@
+Config = {}
+
+Config.KMH = true ------ If set to false it uses mph
+Config.KMHspeed = 20 ------- how many kmh you need to stunt
+Config.MPHspeed = 20 ------- how many mph you need to stunt requires Config.KMH off
+
+Config.Bikes = {
+    '2015crf450',
+    'SANCHEZ01',
+    'BF400',
+    'AKUMA',
+    'CLIFFHANGER',
+    'ESSKEY',
+    'ENDURO',
+    'FAGGIO',
+    'VORTEX',
+    'DOUBLE',
+    'VADER',
+    'DEFILER',
+    'CARBONRS',
+    'NEMESIS',
+    'BATI2',
+    'BATI',
+    'RUFFIAN',
+    'PCJ',
+    'SANCHEZ2'  
+}
