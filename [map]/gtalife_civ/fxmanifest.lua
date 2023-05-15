648271c993cd04693dcbb4fba08217b430bfc185
Benjamin7222 <benjidu722@gmail.com>
2023-05-15 11:38:29 +0200

    vetement

---
+++ [map]/gtalife_civ/fxmanifest.lua
@@ -0,0 +1,16 @@
+-- Generated with AltTool
+
+fx_version 'cerulean'
+game { 'gta5' }
+
+files {
+  'mp_m_freemode_01_mp_m_gtalife_civ.meta',
+  'mp_m_freemode_01_mp_m_gtalife_civ2.meta',
+  'mp_f_freemode_01_mp_f_gtalife_civ.meta',
+  'mp_f_freemode_01_mp_f_gtalife_civ2.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_gtalife_civ.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_gtalife_civ2.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_gtalife_civ.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_gtalife_civ2.meta'
